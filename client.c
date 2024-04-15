f1939b3e038b8b90118d70fb0a9d6ad6fa0aa775
tropikss <Mathis>
2024-04-15 21:40:28 +0200

    ajout commentaires

--- client.c
+++ client.c
@@ -6,6 +6,9 @@
 
 #include <string.h>
 
+// pareil que pour "serveur.sh", faites "./client.sh", ca recupere automatiquement votre IP (pour les tests)
+// et ca attribue tout seul un nouveau port
+
 int main(int argc, char *argv[]) {
 
   printf("Début programme\n");
@@ -17,14 +20,15 @@ int main(int argc, char *argv[]) {
   inet_pton(AF_INET,argv[1],&(aS.sin_addr)) ;
   aS.sin_port = htons(atoi(argv[2])) ;
   socklen_t lgA = sizeof(struct sockaddr_in) ;
-  if(connect(dS, (struct sockaddr *) &aS, lgA) == -1) {
-    printf("Pas de serveur trouvé\n");
+
+  if(connect(dS, (struct sockaddr *) &aS, lgA) == -1) { // tentative connexion au serveur
+    printf("Pas de serveur trouvé\n"); // echec
     exit(1);
   }
-  printf("Socket Connecté\n");
+  printf("Socket Connecté\n"); // reussite
 
   int role;
-  recv(dS, &role, sizeof(role), 0);
+  recv(dS, &role, sizeof(role), 0); // on recupere le role du client, 0 = envoi en premier, 1 = ecoute en premier
 
   if(role == 0) {
     printf("Je suis le premier client\n");
@@ -32,27 +36,28 @@ int main(int argc, char *argv[]) {
     printf("Je suis le deuxieme client\n");
   }
 
-  int actif = 1;
-  int MSG_SIZE = 32;
+  int actif = 1; // actif : etat du client
+  int MSG_SIZE = 32; // tailel des msg envoyé, a changer pa la suitep our mettre un truc variable qu'on recupere de puis le serveur
 
   while (actif) {
 
-    if(role == 0) {
+    if(role == 0) { // envoi
       char *msg = (char*)malloc(MSG_SIZE);
       printf("< ");
       fgets(msg, MSG_SIZE, stdin);
-      if(send(dS, msg, MSG_SIZE, 0) != -1) {
-        recv(dS, &actif, sizeof(int), 0);
-        role = 1;
+      if(send(dS, msg, MSG_SIZE, 0) != -1) { // si le send n'echoue pas
+        recv(dS, &actif, sizeof(int), 0); // on met dans actif le retour du serveur -> c'est le serveur
+        // qui determine si on doit arreter la connexion ou pas, donc le serveur renvoie l'etat que doit avoir actif du client
+        role = 1; // on change les roles, le sender devient receiver et inversemment
       } else {
         printf("erreur envoi\n");
       }
     } else {
       char *msg = (char*)malloc(MSG_SIZE);
-      recv(dS, msg, MSG_SIZE, 0);
-      recv(dS, &actif, sizeof(int), 0);
+      recv(dS, msg, MSG_SIZE, 0); // on recupere le message
+      recv(dS, &actif, sizeof(int), 0); // et pareil, on modifie actif suivant la reponse serveur
       printf("> %s", msg);
-      role = 0;
+      role = 0; // on change les roles
     }
   }
   
--- serveur.c
+++ serveur.c
@@ -4,9 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int msg_send(int dS, char * msg, int size) {
-  return(send(dS, msg, size, 0) == size);
-}
+// Vous pouvez lancer le programme avec "./serveur.sh", c'est un programme qui attribue automatiquement
+// un nouveau port a serveur et client, bien sur le meme entre eux
 
 int main(int argc, char *argv[]) {
   
@@ -36,14 +35,14 @@ int main(int argc, char *argv[]) {
   struct sockaddr_in sock_client2 ;
   socklen_t lg2 = sizeof(struct sockaddr_in) ;
 
-  int client1 = accept(dS, (struct sockaddr*) &sock_client1,&lg1) ;
+  int client1 = accept(dS, (struct sockaddr*) &sock_client1,&lg1) ; // on recupere le client 1 
   if(client1 > 0) {
     printf("Client 1 connecté\n");
     int premier = 0;
     send(client1, &premier, sizeof(int), 0); // on transmet a 1 qu'il enverra en premier
   }
 
-  int client2 = accept(dS, (struct sockaddr*) &sock_client2,&lg2);
+  int client2 = accept(dS, (struct sockaddr*) &sock_client2,&lg2); // on recupere le client 2
     if(client2 > 0) {
       printf("Client 2 connecté\n\n");
       int deuxieme = 1;
@@ -53,26 +52,27 @@ int main(int argc, char *argv[]) {
   int actif = 1;
   int MSG_SIZE = 32;
   char *msg = (char*)malloc(MSG_SIZE);
-  int sender = client1;
-  int receiver = client2;
+
+  int sender = client1; // personne qui enverra en premier, ici client1 par default
+  int receiver = client2; // pareil amis client 2 et recevoir
 
   while(actif) {
 
     msg = (char*)malloc(MSG_SIZE);
     recv(sender, msg, MSG_SIZE, 0);
     printf("> %s",msg);
-    if(send(receiver, msg, MSG_SIZE, 0) != -1) {
-      if(strcmp(msg, "fin\n\0") == 0) {
-        actif = 0;
+    if(send(receiver, msg, MSG_SIZE, 0) != -1) { // si l'envoi n'echoue pas
+      if(strcmp(msg, "fin\n\0") == 0) { // si le message recu vaut "fin", y'avait \n et \0 finalement a la fin
+        actif = 0; // si y'a fin alors on arrete tout
       }
-      send(sender, &actif, sizeof(int), 0);
+      send(sender, &actif, sizeof(int), 0); // et on envoie au deux client d'arreter aussi
       send(receiver, &actif, sizeof(int), 0);
     } else {
       printf("erreur envoi\n");
     }
 
-    int temp = sender;
-    sender = receiver;
+    int temp = sender; // on stocke le sender le temps de la reatribution
+    sender = receiver; // et on echange l'envoyeur et le receveur
     receiver = temp;
   }
 
