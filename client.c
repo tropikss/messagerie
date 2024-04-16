24a00d6229f79c85a717038a74155fc4b3266a0f
neptia <poomedyrungen@gmail.com>
2024-04-17 00:45:28 +0200

    "Adjust output"

--- client.c
+++ client.c
@@ -10,7 +10,6 @@
 
 int client_socket;
 
-// Fonction pour recevoir et afficher les messages du serveur
 void *receive_message(void *arg) {
     while (1) {
         char message[MSG_SIZE];
@@ -21,17 +20,6 @@ void *receive_message(void *arg) {
         }
         message[bytes_received] = '\0';
         printf("< %s", message);
-    }
-    return NULL;
-}
-
-// Fonction pour la saisie et l'envoi de messages au serveur
-void *send_message(void *arg) {
-    while (1) {
-        char message[MSG_SIZE];
-        printf("> ");
-        fgets(message, MSG_SIZE, stdin);
-        send(client_socket, message, strlen(message), 0);
     }
     return NULL;
 }
@@ -61,14 +49,15 @@ int main(int argc, char *argv[]) {
 
     printf("Connected to server. Start typing messages...\n");
 
-    // Création des threads pour la réception et l'envoi de messages
-    pthread_t receive_tid, send_tid;
+    pthread_t receive_tid;
     pthread_create(&receive_tid, NULL, receive_message, NULL);
-    pthread_create(&send_tid, NULL, send_message, NULL);
 
-    // Attente de la fin des threads
-    pthread_join(receive_tid, NULL);
-    pthread_join(send_tid, NULL);
+    char message[MSG_SIZE];
+    while (1) {
+        printf("> ");
+        fgets(message, MSG_SIZE, stdin);
+        send(client_socket, message, strlen(message), 0);
+    }
 
     close(client_socket);
     return EXIT_SUCCESS;
