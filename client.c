32fd2ffc8676df5a985eb33824373d8d1d45325e
neptia <poomedyrungen@gmail.com>
2024-04-17 01:29:50 +0200

    "ajout des commentaires"

--- client.c
+++ client.c
@@ -8,57 +8,54 @@
 
 #define MSG_SIZE 500
 
-int client_socket;
+int dS;
 
-void *receive_message(void *arg) {
-    while (1) {
-        char message[MSG_SIZE];
-        int bytes_received = recv(client_socket, message, MSG_SIZE, 0);
-        if (bytes_received <= 0) {
-            printf("Server disconnected.\n");
-            break;
-        }
-        message[bytes_received] = '\0';
-        printf("< %s", message);
-    }
-    return NULL;
+// pareil que pour "serveur.sh", faites "./client.sh", ca recupere automatiquement votre IP (pour les tests)
+// et ca attribue tout seul un nouveau port
+
+void *receive_message(void *arg) { //processus pour recevoir les messages
+  while (1) {
+    char message[MSG_SIZE];
+      int bytes_received = recv(dS, message, MSG_SIZE, 0);
+      if (bytes_received <= 0) {//si le client ne reçoit plus du serveur
+          printf("Server disconnected.\n");
+          break;
+      }
+      message[bytes_received] = '\0';
+      printf("> %s", message);
+  }
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <server_ip> <server_port>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+  printf("Début programme\n");
+  dS = socket(PF_INET, SOCK_STREAM, 0);
+  printf("Socket Créé\n");
+   
+  struct sockaddr_in aS;
+  aS.sin_family = AF_INET;
+  inet_pton(AF_INET,argv[1],&(aS.sin_addr)) ;
+  aS.sin_port = htons(atoi(argv[2])) ;
+  socklen_t lgA = sizeof(struct sockaddr_in) ;
+   
+  if(connect(dS, (struct sockaddr *) &aS, lgA) == -1) { // tentative connexion au serveur
+    printf("Pas de serveur trouvé\n"); // echec
+    exit(1);
+  }
+  
+  printf("Socket Connecté\n"); // reussite
+  
+  //Création de thread pour recevoir les messages
+  pthread_t receive_tid;
+  pthread_create(&receive_tid, NULL, receive_message, NULL);
 
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("socket");
-        return EXIT_FAILURE;
-    }
-
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr(argv[1]);
-    server_address.sin_port = htons(atoi(argv[2]));
-
-    if (connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
-        perror("connect");
-        close(client_socket);
-        return EXIT_FAILURE;
-    }
-
-    printf("Connected to server. Start typing messages...\n");
-
-    pthread_t receive_tid;
-    pthread_create(&receive_tid, NULL, receive_message, NULL);
-
-    char message[MSG_SIZE];
+  char message[MSG_SIZE];
     while (1) {
-        printf("> ");
-        fgets(message, MSG_SIZE, stdin);
-        send(client_socket, message, strlen(message), 0);
+      printf("< ");
+      fgets(message, MSG_SIZE, stdin);
+      send(dS, message, strlen(message), 0);
     }
 
-    close(client_socket);
-    return EXIT_SUCCESS;
+  close(dS);
+  return EXIT_SUCCESS;
 }
--- serveur.c
+++ serveur.c
@@ -12,82 +12,77 @@
 int client_sockets[MAX_CLIENTS];
 pthread_t client_threads[MAX_CLIENTS];
 
-// Fonction pour relayer les messages entre les clients
+// Vous pouvez lancer le programme avec "./serveur.sh", c'est un programme qui attribue automatiquement
+// un nouveau port a serveur et client, bien sur le meme entre eux
+
+
+//Fonction pour relayer les messages entre les clients
 void *relay_messages(void *arg) {
-    int sender_index = *((int *) arg);
-    int receiver_index = (sender_index + 1) % MAX_CLIENTS;
+  int sender_index = *((int *) arg);
+  int receiver_index = (sender_index + 1) % MAX_CLIENTS;
 
-    char message[MSG_SIZE];
-    int bytes_received;
+  char message[MSG_SIZE];
+  int bytes_received;
 
-    while ((bytes_received = recv(client_sockets[sender_index], message, MSG_SIZE, 0)) > 0) {
-        message[bytes_received] = '\0';
-
-        send(client_sockets[receiver_index], message, strlen(message), 0);
+  while ((bytes_received = recv(client_sockets[sender_index], message, MSG_SIZE, 0)) > 0) {
+    message[bytes_received] = '\0';
+    send(client_sockets[receiver_index], message, strlen(message), 0);
+    // Vérifie si le message reçu est "fin"
+    if (strcmp(message, "fin\n") == 0) {
+      printf("Client %d a terminé la conversation.\n", sender_index + 1);
+      break; // Sort de la boucle while
     }
-
-    // Si le client se déconnecte, fermer le socket et arrêter le thread
-    close(client_sockets[sender_index]);
-    client_sockets[sender_index] = -1;
-    printf("Client %d disconnected.\n", sender_index + 1);
-
-    return NULL;
+  }
+  //Si le client se déconnecte, ferme le socket et arrête le thread
+  close(client_sockets[sender_index]);
+  client_sockets[sender_index] = -1;
+  printf("Client %d disconnected.\n", sender_index + 1);
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <port>\n", argv[0]);
-        return EXIT_FAILURE;
+  printf("Début programme\n");
+
+  int dS = socket(PF_INET, SOCK_STREAM, 0);
+  printf("Socket Créé\n");
+
+  struct sockaddr_in ad;
+  ad.sin_family = AF_INET;
+  ad.sin_addr.s_addr = INADDR_ANY ;
+  ad.sin_port = htons(atoi(argv[1])) ; // PORT SERVEUR
+  int res = bind(dS, (struct sockaddr*)&ad, sizeof(ad));
+  if(res == -1) {
+    perror("Erreur bind");
+    exit(1);
+  }
+  printf("Socket Nommé\n");
+
+  listen(dS, 7) ;
+  printf("Mode écoute\n");
+
+  //Accepte les connexions des clients
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    struct sockaddr_in client_address;
+    socklen_t client_address_len = sizeof(client_address);
+    client_sockets[i] = accept(dS, (struct sockaddr *) &client_address, &client_address_len);
+    if (client_sockets[i] == -1) {
+      perror("accept");
+      close(dS);
+      return EXIT_FAILURE;
     }
+    printf("Client %d connected.\n", i + 1);
 
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        perror("socket");
-        return EXIT_FAILURE;
-    }
+    //Crée un thread pour chaque client
+    int *client_index_ptr = malloc(sizeof(int));
+    *client_index_ptr = i;
+    pthread_create(&client_threads[i], NULL, relay_messages, client_index_ptr);
+  }
 
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(atoi(argv[1]));
+  //Attend la fin de tous les threads clients
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+      pthread_join(client_threads[i], NULL);
+  }
 
-    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
-        perror("bind");
-        close(server_socket);
-        return EXIT_FAILURE;
-    }
-
-    if (listen(server_socket, MAX_CLIENTS) == -1) {
-        perror("listen");
-        close(server_socket);
-        return EXIT_FAILURE;
-    }
-
-    printf("Server listening on port %s...\n", argv[1]);
-
-    // Accepter les connexions des clients
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        struct sockaddr_in client_address;
-        socklen_t client_address_len = sizeof(client_address);
-        client_sockets[i] = accept(server_socket, (struct sockaddr *) &client_address, &client_address_len);
-        if (client_sockets[i] == -1) {
-            perror("accept");
-            close(server_socket);
-            return EXIT_FAILURE;
-        }
-        printf("Client %d connected.\n", i + 1);
-
-        // Créer un thread pour chaque client
-        int *client_index_ptr = malloc(sizeof(int));
-        *client_index_ptr = i;
-        pthread_create(&client_threads[i], NULL, relay_messages, client_index_ptr);
-    }
-
-    // Attendre la fin de tous les threads clients
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        pthread_join(client_threads[i], NULL);
-    }
-
-    close(server_socket);
-    return EXIT_SUCCESS;
+  close(dS);
+  return EXIT_SUCCESS;
 }
