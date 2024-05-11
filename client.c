ccce9f7517505ffb87c2bc91759d1872f85de699
neptia <poomedyrungen@gmail.com>
2024-05-11 17:48:44 +0200

    "Sprint 2"

--- client.c
+++ client.c
@@ -1,75 +1,105 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <pthread.h>
 
-#define MSG_SIZE 32
+//------------------------------------------
+#define MSG_SIZE 100 // Taille max du message
+//------------------------------------------
+int flag = 0;        // Gestion du signal
+int dS = 0; // Descripteur de fichier du socket
+char nom[32];        // Nom du client
 
 // pareil que pour "serveur.sh", faites "./client.sh", ca recupere automatiquement votre IP (pour les tests)
 // et ca attribue tout seul un nouveau port
 
-typedef struct
+// Client appuie sur Ctrl+C pour terminer le programme
+void catch_ctrl_c_and_exit(int sig)
 {
-  int type;
-  int taille;
-  union {
-    int i;
-    char msg[MSG_SIZE];
-  } info;
-} data;
-
-void* msg_recv(void* args) {
-    int* dS = (int*)args;
-    data recv_data;
-
-    while(1) {
-      recv(*dS, &recv_data, sizeof(data), 0);
-      //J'alloue la taille du message
-      char* msg = (char*)malloc(recv_data.taille); 
-
-      if(recv_data.type == 0) { // int
-        pthread_exit(NULL);
-      } else if(recv_data.type == 1) { // char[]
-      // Copier le message de recv_data.info.msg dans msg
-        strncpy(msg, recv_data.info.msg, MSG_SIZE);
-        printf("> %s", recv_data.info.msg); // Imprimer le message
-      }
-      free(msg);
-    }
-    return NULL;
+  flag = 1;
 }
 
-void* msg_send(void* args) {
-  data send_data;
+//***********************OUTDATE**************************
+// typedef struct
+// {
+//   int type;
+//   int taille;
+//   union
+//   {
+//     int i;
+//     char msg[MSG_SIZE];
+//   } info;
+// } data;
 
-  int* dS = (int*)args;
+// Gère la réception de messages
+void msg_recv()
+{
+  char msg[MSG_SIZE] = {};
 
-  while(1) {
-    int taille;
-
-    char *msg = (char*)malloc(MSG_SIZE);
-
-    printf("< ");
-    fgets(msg, MSG_SIZE, stdin);
-    taille = strlen(msg);
-    //Envoie de la taille du message d'abord
-    send(*dS, &taille, sizeof(int) , 0);
-    //Puis envoie le message
-    send(*dS, msg, sizeof(msg), 0);
-
-    free(msg);
+  while (1)
+  {
+    int response = recv(dS, msg, MSG_SIZE, 0);
+    if (response > 0)
+    {
+      printf("%s", msg);
+    }
+    else if (response == 0)
+    {
+      break;
+    }
+    else
+    {
+      // -1
+    }
+    memset(msg, 0, sizeof(msg));
   }
-  pthread_exit(NULL);
+}
+
+// Gère l'envoi de messages
+void msg_send()
+{
+  char msg[MSG_SIZE] = {};
+  char buffer[MSG_SIZE + 64] = {};
+
+  while (1)
+  {
+    printf("%s", "> ");
+    fgets(msg, MSG_SIZE, stdin);
+    int i;
+    for (i = 0; i < MSG_SIZE; i++)
+    { // trim \n
+      if (msg[i] == '\n')
+      {
+        msg[i] = '\0';
+        break;
+      }
+    }
+    if (strcmp(msg, "fin") == 0)
+    {
+      break;
+    }
+    else
+    {
+      sprintf(buffer, "%s: %s\n", nom, msg);
+      send(dS, buffer, strlen(buffer), 0);
+      printf("Test\n");
+    }
+    memset(msg, 0, MSG_SIZE);
+    memset(buffer, 0, MSG_SIZE + 32);
+  }
+  catch_ctrl_c_and_exit(2);
 }
 
 int main(int argc, char *argv[])
 {
-
+  dS = socket(AF_INET, SOCK_STREAM, 0);
   printf("Début programme\n");
-  int dS = socket(PF_INET, SOCK_STREAM, 0);
   if (dS == -1)
   {
     perror("Erreur lors de la création de la socket");
@@ -83,6 +113,9 @@ int main(int argc, char *argv[])
   aS.sin_port = htons(atoi(argv[2]));
   socklen_t lgA = sizeof(struct sockaddr_in);
 
+  /* Signal */
+  signal(SIGINT, catch_ctrl_c_and_exit);
+
   if (connect(dS, (struct sockaddr *)&aS, lgA) == -1) // tentative connexion au serveur
   {
     printf("Pas de serveur trouvé\n"); // echec
@@ -90,31 +123,69 @@ int main(int argc, char *argv[])
   }
   printf("Socket Connecté\n"); // reussite
 
-  pthread_t sender;
-  pthread_t receiver;
-  int state = 1;
-  data recv_data;
+  //***********************OUTDATE**************************
+  // pthread_t sender;
+  // pthread_t receiver;
+  // int state = 1;
+  // data recv_data;
 
-  if (pthread_create(&sender, NULL, msg_send, (void *)&dS) != 0)
+  // if (pthread_create(&sender, NULL, msg_send, (void *)&dS) != 0)
+  // {
+  //   printf("erreur thread sender\n");
+  // }
+
+  // if (pthread_create(&receiver, NULL, msg_recv, (void *)&dS) != 0)
+  // {
+  //   printf("erreur thread receiver\n");
+  // }
+
+  // if (pthread_join(receiver, NULL) != 0)
+  // {
+  //   printf("Erreur lors de l'attente du thread 'receiver'");
+  //   exit(EXIT_FAILURE);
+  // }
+
+  // if (pthread_cancel(sender) != 0)
+  // {
+  //   perror("Erreur lors de l'annulation du thread 'sender'");
+  //   exit(EXIT_FAILURE);
+  // }
+
+  printf("Veuillez entrer votre nom: ");
+  fgets(nom, 32, stdin);
+  int i;
+  for (i = 0; i < strlen(nom); i++)
+  { // trim \n
+    if (nom[i] == '\n')
+    {
+      nom[i] = '\0';
+      break;
+    }
+  }
+  send(dS, nom, 32, 0); // Envoie le nom au serveur
+
+  printf("*** Bienvenue ***\n");
+
+  pthread_t send_msg_thread;
+  if (pthread_create(&send_msg_thread, NULL, (void *)msg_send, NULL) != 0)
   {
-    printf("erreur thread sender\n");
+    printf("ERREUR: pthread\n");
+    return EXIT_FAILURE;
   }
 
-  if (pthread_create(&receiver, NULL, msg_recv, (void *)&dS) != 0)
+  pthread_t recv_msg_thread;
+  if (pthread_create(&recv_msg_thread, NULL, (void *)msg_recv, NULL) != 0)
   {
-    printf("erreur thread receiver\n");
+    printf("ERREUR: pthread\n");
+    return EXIT_FAILURE;
   }
 
-  if (pthread_join(receiver, NULL) != 0)
+  while (1)
   {
-    printf("Erreur lors de l'attente du thread 'receiver'");
-    exit(EXIT_FAILURE);
-  }
-
-  if (pthread_cancel(sender) != 0)
-  {
-    perror("Erreur lors de l'annulation du thread 'sender'");
-    exit(EXIT_FAILURE);
+    if (flag)
+    {
+      break;
+    }
   }
 
   shutdown(dS, 2);
--- serveur.c
+++ serveur.c
@@ -7,169 +7,304 @@
 #include <signal.h>
 #include <unistd.h>
 
-#define MSG_SIZE 32
-#define MAX_CLIENT 3
-#define MAX_NOM 20
+//------------------------------------------
+#define MSG_SIZE 100   // Taille max du message
+#define MAX_CLIENT 10 // Nombre max de clients
+//------------------------------------------
+static unsigned int nb_client = 0; // Nombre de clients connectés
+static int uid = 10;               // Taille des identifiants(unique) de chaque client
 
 // Vous pouvez lancer le programme avec "./serveur.sh", c'est un programme qui attribue automatiquement
 // un nouveau port au serveur et au client, bien sur le meme entre eux
 
-void sigHandler(int signo) {
-    if (signo == SIGUSR1) {
-        printf("Au moins un des threads s'est terminé\n");
-        exit(EXIT_SUCCESS);
-    }
-}
+//***********************OUTDATE**************************
+// void sigHandler(int signo) {
+//     if (signo == SIGUSR1) {
+//         printf("Au moins un des threads s'est terminé\n");
+//         exit(EXIT_SUCCESS);
+//     }
+// }
 
-typedef struct {
-  int *tab_client_client;
-  pthread_mutex_t *mutex_client;
-  int id_client;
-  int *tab_size;
-  char *nom;
+// Structure du client - Information sur eux
+typedef struct
+{
+  struct sockaddr_in address; // Adresse du client(adresse IP et le port du client)
+  int sockID;                 // Identifiant de la socket du client
+  int id_client;              // Identifiant unique du client
+  char nom[64];               // Nom du client
 } client;
 
-typedef struct {
-  int type;
-  int taille; //ajout de la taille du message pas généré par chatGPT :P MathisssEEEuuu (sais qu'il va même pas regardez mon code)
-  union {
-    int i;
-    char msg[MSG_SIZE];
-  } info;
-} data;
+client *clientsTab[MAX_CLIENT]; // Tableau de tous les clients connectés
 
-void* new_client(void* args) {
-  data send_data;
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER; // Initialisation d'un mutex pour synchroniser l'accès concurrentiel aux données des clients
 
-  client* data_client = (client*)args;
-  int id_client = data_client->id_client;
-  int *tab_client = data_client->tab_client_client;
-  int *tab_size = data_client->tab_size;
-  pthread_mutex_t *mutex_client = data_client->mutex_client;
-  char *nom = data_client->nom;
+//***********************OUTDATE**************************
+// typedef struct {
+//   int type;
+//   int taille; //ajout de la taille du message pas généré par chatGPT :P MathisssEEEuuu (sais qu'il va même pas regardez mon code)
+//   union {
+//     int i;
+//     char msg[MSG_SIZE];
+//   } info;
+// } data;
 
-  int state = 1;
+// Fonction qui ajoute un nouveau client
+void ajout_client(client *nouveau_client)
+{
+  pthread_mutex_lock(&clients_mutex);
+  for (int i = 0; i < MAX_CLIENT; ++i)
+  {
+    if (!clientsTab[i])
+    {
+      clientsTab[i] = nouveau_client;
+      break;
+    }
+  }
+  pthread_mutex_unlock(&clients_mutex);
+}
 
-  while(state) {
-    int taille;
-    //reçoie la taille du message d'abord;
-    recv(id_client, &taille, sizeof(int), 0);
-    send_data.taille = taille;
-
-    //J'alloue la taille du message
-    char * msg = (char*)malloc(taille);
-    
-    //puis reçoie le message du client
-    recv(id_client, msg, MSG_SIZE, 0);
-    printf("%s: %s",nom,msg);
-    send_data.type = 1;
-    strncpy(send_data.info.msg, msg, MSG_SIZE);
-
-    pthread_mutex_lock(mutex_client);
-    for(int i = 0; i < *tab_size; i++) {
-      if(tab_client[i] != id_client) {
-        send(tab_client[i], &send_data, sizeof(data), 0);
+// Fonction qui retire un client du tableau à l'aide de son identifiant
+void retire_client(int uid)
+{
+  pthread_mutex_lock(&clients_mutex);
+  for (int i = 0; i < MAX_CLIENT; ++i)
+  {
+    if (clientsTab[i])
+    {
+      if (clientsTab[i]->id_client == uid)
+      {
+        clientsTab[i] = NULL;
+        break;
       }
     }
-    pthread_mutex_unlock(mutex_client);
+  }
+  pthread_mutex_unlock(&clients_mutex);
+}
 
-    state = strcmp(msg, "fin\n\0"); // si y'a fin alors on arrete tout
+// Fonction qui envoie un message à tout le monde sauf au sender
+void send_message(char *s, int uid)
+{
+  pthread_mutex_lock(&clients_mutex);
+  for (int i = 0; i < MAX_CLIENT; ++i)
+  {
+    if (clientsTab[i])
+    {
+      if (clientsTab[i]->id_client != uid) // vérifie si cela n'est id du sender
+      {
+        if (write(clientsTab[i]->sockID, s, strlen(s)) < 0)
+        {
+          perror("ERREUR: Envoie du message échoué");
+          break;
+        }
+      }
+    }
+  }
+  pthread_mutex_unlock(&clients_mutex);
+}
 
-    free(msg);
+// Gère la communication entre les clients (envoie et réception)
+// Executer dans un thread séparé pour chaque client
+void *new_client(void *args)
+{
+  //***********************OUTDATE**************************
+  // data send_data;
+  // client *data_client = (client *)args;
+  // int id_client = data_client->id_client;
+  // int *tab_client = data_client->tab_client_client;
+  // int *tab_size = data_client->tab_size;
+  // pthread_mutex_t *mutex_client = data_client->mutex_client;
+  // char *nom = data_client->nom;
+
+  char message[MSG_SIZE];               // Message sortant
+  char nom[64];                         // Nom du client
+  int state = 0;                        // Indique si le client quitte la convo
+  nb_client++;                          // Incrémente le nombre de clients
+  client *data_client = (client *)args; // Pointeur de type client pour accéder aux données du client
+
+  // Vérification du nom du client
+  if (recv(data_client->sockID, nom, 64, 0) <= 0 || strlen(nom) < 2 || strlen(nom) >= 64 - 1)
+  {
+    printf("Taille incorrecte.\n");
+    state = 1;
+  }
+  else
+  {
+    strcpy(data_client->nom, nom);
+    sprintf(message, "%s a rejoint le chat\n", data_client->nom);
+    printf("%s", message);
+    send_message(message, data_client->id_client);
   }
 
-  send_data.type = 0;
-  send_data.info.i = 0;
+  while (1)
+  {
+    if (state) // le client doit quiiter le chat
+    {
+      break;
+    }
 
-  pthread_mutex_lock(mutex_client);
-  for(int i = 0; i < *tab_size; i++) {
-    send(tab_client[i], &send_data, sizeof(data), 0);
+    // Réception d'un message du client
+    int receive = recv(data_client->sockID, message, MSG_SIZE, 0);
+    if (receive > 0)
+    {
+      if (strlen(message) > 0)
+      {
+        send_message(message, data_client->id_client);
+        int i;
+        for (i = 0; i < strlen(message); i++)
+        { // trim \n
+          if (message[i] == '\n')
+          {
+            message[i] = '\0';
+            break;
+          }
+        }
+        printf("%s -> %s\n", message, data_client->nom);
+      }
+    }
+    else if (receive == 0 || strcmp(message, "fin") == 0)
+    {
+      sprintf(message, "%s a quitté le chat\n", data_client->nom);
+      printf("%s", message);
+      send_message(message, data_client->id_client);
+      state = 1;
+    }
+    else
+    {
+      printf("ERREUR: -1\n");
+      state = 1;
+    }
+    memset(message, 0, MSG_SIZE);
   }
-  pthread_mutex_unlock(mutex_client);
 
-  pthread_kill(pthread_self(), SIGUSR1);
+  close(data_client->sockID);            // Fermeture du socket du client
+  retire_client(data_client->id_client); // Suppression du client de la file d'attente
+  free(data_client);                     // Libération de la mémoire
+  nb_client--;                           // Décrémentation du nombre total de clients
+  pthread_detach(pthread_self());
 
   return NULL;
 }
 
-int main(int argc, char *argv[]) {
-  
+int main(int argc, char *argv[])
+{
+
   printf("Début programme\n");
 
   int dS = socket(PF_INET, SOCK_STREAM, 0);
   printf("Socket Créé\n");
 
+  /* Socket settings */
   struct sockaddr_in ad;
   ad.sin_family = AF_INET;
-  ad.sin_addr.s_addr = INADDR_ANY ;
-  ad.sin_port = htons(atoi(argv[1])) ; // PORT SERVEUR
-  int res = bind(dS, (struct sockaddr*)&ad, sizeof(ad));
-  if(res == -1) {
+  ad.sin_addr.s_addr = INADDR_ANY;
+  ad.sin_port = htons(atoi(argv[1])); // PORT SERVEUR
+
+  /* Ignore pipe signals */
+  signal(SIGPIPE, SIG_IGN);
+  int option = 1;
+  if (setsockopt(dS, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char *)&option, sizeof(option)) < 0)
+  {
+    perror("ERROR: setsockopt failed");
+    return EXIT_FAILURE;
+  }
+
+  /* Bind */
+  int res = bind(dS, (struct sockaddr *)&ad, sizeof(ad));
+  if (res == -1)
+  {
     perror("Erreur bind");
     exit(1);
   }
   printf("Socket Nommé\n");
 
-  listen(dS, 7) ;
+  /* Listen */
+  listen(dS, 7);
   printf("Mode écoute\n");
 
-  int state = 1;
-  int *nb_client = (int*)malloc(sizeof(int));
-  *nb_client = 0;
+  //***********************OUTDATE**************************
+  // int *nb_client = (int *)malloc(sizeof(int));
+  // *nb_client = 0;
 
-  int *tab_client = (int*)malloc(MAX_CLIENT * sizeof(int)); // création du tableau d'id client
-  pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+  // int *tab_client = (int *)malloc(MAX_CLIENT * sizeof(int)); // création du tableau d'id client
+  // pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-  pthread_t *tab_thread = (pthread_t*)malloc(MAX_CLIENT * sizeof(pthread_t));
+  // pthread_t *tab_thread = (pthread_t *)malloc(MAX_CLIENT * sizeof(pthread_t));
+
+  // struct sockaddr_in sock_client;
+  // socklen_t lg = sizeof(struct sockaddr_in);
+
+  // client *shared_client = (client *)malloc(sizeof(client));
+  // shared_client->mutex_client = &mutex;
+  // shared_client->tab_client_client = tab_client;
+
+  // struct sigaction sa;
+  // sa.sa_handler = sigHandler;
+  // sigemptyset(&sa.sa_mask);
+  // sa.sa_flags = 0;
+  // sigaction(SIGUSR1, &sa, NULL);
+
+  // printf("Initialisation réussie\n");
 
   struct sockaddr_in sock_client;
-  socklen_t lg = sizeof(struct sockaddr_in);
+  pthread_t tid;
 
-  client *shared_client = (client*)malloc(sizeof(client));
-  shared_client->mutex_client = &mutex;
-  shared_client->tab_client_client = tab_client;
-
-  struct sigaction sa;
-  sa.sa_handler = sigHandler;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = 0;
-  sigaction(SIGUSR1, &sa, NULL);
-
-  printf("Initialisation réussie\n");
-  
-  while(state) {
-    int request = accept(dS, (struct sockaddr*) &sock_client, &lg); // on attend une connexion
+  while (1)
+  {
+    socklen_t lg = sizeof(sock_client);
+    int request = accept(dS, (struct sockaddr *)&sock_client, &lg); // on attend une connexion
     printf("Demande connexion\n");
-    if(*nb_client < MAX_CLIENT) { 
-      if(request > 0) { // on vérifie que la connexion s'est bien faite
+    if (nb_client < MAX_CLIENT) // vérifie si on atteint pas le nb max
+    {
+      if (request > 0)
+      { // on vérifie que la connexion s'est bien faite
         printf("Connexion possible\n");
 
-        pthread_mutex_lock(&mutex);
-        tab_client[*nb_client] = request;
-        printf("Client %d connecté ", *nb_client+1);
-        shared_client->id_client = request;
-        shared_client->tab_size = nb_client;
-        *nb_client += 1;
-        char *nom = (char*)malloc(MAX_NOM * sizeof(char));
-        sprintf(nom, "client n%d", *nb_client);
-        shared_client->nom = nom;
+        // Alloue de la mémoire pour un nouveau client, puis initialise ses données
+        client *newClient = (client *)malloc(sizeof(client));
+        newClient->address = sock_client;
+        newClient->sockID = request;
+        newClient->id_client = uid++;
 
-        pthread_mutex_unlock(&mutex);
-        
-        if(pthread_create(&tab_thread[*nb_client], NULL, new_client, shared_client) != 0) {
-          printf("avec erreur\n");
-        } else {
-          printf("avec succès\n");
-        }
-      } else {
+        // Ajoute le nouveau client à la file d'attente des client
+        ajout_client(newClient);
+        // Crée un thread pour gérer la communication avec le client
+        pthread_create(&tid, NULL, &new_client, (void *)newClient);
+        sleep(1);
+
+        //***********************OUTDATE**************************
+        // pthread_mutex_lock(&mutex);
+        // tab_client[*nb_client] = request;
+        // printf("Client %d connecté ", *nb_client + 1);
+        // shared_client->id_client = request;
+        // shared_client->tab_size = nb_client;
+        // *nb_client += 1;
+        // char *nom = (char *)malloc(MAX_NOM * sizeof(char));
+        // sprintf(nom, "client n%d", *nb_client);
+        // shared_client->nom = nom;
+
+        // pthread_mutex_unlock(&mutex);
+
+        // if (pthread_create(&tab_thread[*nb_client], NULL, new_client, shared_client) != 0)
+        // {
+        //   printf("avec erreur\n");
+        // }
+        // else
+        // {
+        //   printf("avec succès\n");
+        // }
+      }
+      else
+      {
         printf("Erreur connexion\n");
       }
-    } else {
+    }
+    else
+    {
       printf("Nombre maximal de client deja atteint, request refusé\n");
       shutdown(request, 2);
     }
   }
 
-  shutdown(dS, 2) ;
+  shutdown(dS, 2);
   printf("Fin du programme\n");
 }
